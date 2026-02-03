71179122501c789b7592674fa3c7b34bb179f7c2
wibssono <wibssono@gmail.com>
2026-02-03 23:53:33 +0700

    Working on Mock Patient Data Generator

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-from fastapi import FastAPI, Query
-from typing import Annotated
-from pydantic import AfterValidator, BaseModel
+def main():
+    print("Hello hospital-patient-flow project!")
 
-app = FastAPI()
-
-def valid_item_id(item_id: str | None) -> (str | None):
-    if not item_id:
-        return
-    if item_id.startswith(("id-", "us-")):
-        return item_id
-    else:
-        error = "Invalid Item"
-        raise ValueError (error)
-
-class item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-@app.get("/items/")
-# This is a deprecated Query
-async def read_items(q: Annotated[str | None, Query(title="Query asked",
-                                                    max_length=10, 
-                                                    min_length=10,
-                                                    deprecated=True)] = None) -> dict[str, str | None]:
-    return {"Did you ask": q}
-
-@app.get('/itemvalues/{item_id}/')
-async def get_items(q: Annotated[str | None, Query(title="Giving Item", alias="item-give", description="To give back what was taken"))] = None:
+if __name__ == "__main__":
+    main()
